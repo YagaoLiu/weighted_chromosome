98b5c2cbf8ceaf77890aa0aa9a98a8ae28d03c12
YagaoLiu <yagao_liu@outlook.com>
2023-04-06 17:51:06 +0800

    Update massif.sh

--- WST/massif.sh
+++ WST/massif.sh
@@ -1,5 +1,5 @@
-valgrind --tool=massif --time-unit=ms --massif-out-file=massif.out.wst.z2 ./index -t ../chr22.in -z 2 -p ../z2
-valgrind --tool=massif --time-unit=ms --massif-out-file=massif.out.wst.z2 ./index -t ../chr22.in -z 4 -p ../z4
-valgrind --tool=massif --time-unit=ms --massif-out-file=massif.out.wst.z2 ./index -t ../chr22.in -z 8 -p ../z8
-valgrind --tool=massif --time-unit=ms --massif-out-file=massif.out.wst.z2 ./index -t ../chr22.in -z 16 -p ../z16
-valgrind --tool=massif --time-unit=ms --massif-out-file=massif.out.wst.z2 ./index -t ../chr22.in -z 32 -p ../z32
+valgrind --tool=massif --time-unit=ms --massif-out-file=massif.out.wst.z2 ./index -t ../chr22.in -z 2 -p ../patterns/z2
+valgrind --tool=massif --time-unit=ms --massif-out-file=massif.out.wst.z2 ./index -t ../chr22.in -z 4 -p ../patterns/z4
+valgrind --tool=massif --time-unit=ms --massif-out-file=massif.out.wst.z2 ./index -t ../chr22.in -z 8 -p ../patterns/z8
+valgrind --tool=massif --time-unit=ms --massif-out-file=massif.out.wst.z2 ./index -t ../chr22.in -z 16 -p ../patterns/z16
+valgrind --tool=massif --time-unit=ms --massif-out-file=massif.out.wst.z2 ./index -t ../chr22.in -z 32 -p ../patterns/z32
