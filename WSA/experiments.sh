459b44da6d5ac2046c076a77793b00b76f626b94
YagaoLiu <yagao_liu@outlook.com>
2023-04-19 18:28:30 +0800

    efm and sars

---
+++ WSA/experiments.sh
@@ -0,0 +1,5 @@
+ (/usr/bin/time -v ./index -t ../sars.in -z 2 -p ../patterns/z2 -o pm_time/sars_z2time.txt) &> peak_ram/sars_z2peak.txt
+ (/usr/bin/time -v ./index -t ../sars.in -z 4 -p ../patterns/z4 -o pm_time/sars_z4time.txt) &> peak_ram/sars_z4peak.txt
+ (/usr/bin/time -v ./index -t ../sars.in -z 8 -p ../patterns/z8 -o pm_time/sars_z8time.txt) &> peak_ram/sars_z8peak.txt
+ (/usr/bin/time -v ./index -t ../sars.in -z 16 -p ../patterns/z16 -o pm_time/sars_z16time.txt) &> peak_ram/sars_z16peak.txt
+ (/usr/bin/time -v ./index -t ../sars.in -z 32 -p ../patterns/z32 -o pm_time/sars_z32time.txt) &> peak_ram/sars_z32peak.txt
---
+++ patterns.sh
@@ -0,0 +1,11 @@
+./index -t efm.in -z 8 -o ./patterns/efm_z8
+./index -t efm.in -z 16 -o ./patterns/efm_z16
+./index -t efm.in -z 32 -o ./patterns/efm_z32
+./index -t efm.in -z 64 -o ./patterns/efm_z64
+./index -t efm.in -z 128 -o ./patterns/efm_z128
+
+./index -t sars.in -z 64 -o ./patterns/sars_z64
+./index -t sars.in -z 128 -o ./patterns/sars_z128
+./index -t sars.in -z 256 -o ./patterns/sars_z256
+./index -t sars.in -z 512 -o ./patterns/sars_z512
+./index -t sars.in -z 1024 -o ./patterns/sars_z1024
